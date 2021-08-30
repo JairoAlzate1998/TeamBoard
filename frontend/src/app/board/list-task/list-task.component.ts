ca1a16c29ed824e9f399a466fe0446c83c0257b1
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-30 11:10:38 -0500

    Add list task front

--- frontend/src/app/board/list-task/list-task.component.ts
+++ frontend/src/app/board/list-task/list-task.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { BoardService } from '../../services/board.service';
+import { Router } from '@angular/router';
+import {
+  MatSnackBar,
+  MatSnackBarHorizontalPosition,
+  MatSnackBarVerticalPosition,
+} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-list-task',
@@ -6,10 +13,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-task.component.css']
 })
 export class ListTaskComponent implements OnInit {
+  taskData: any;
+  message: string;
+  horizontalPosition: MatSnackBarHorizontalPosition = 'end';
+  verticalPosition: MatSnackBarVerticalPosition = 'top';
+  durationInSecond: number = 2;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private _boardService: BoardService,
+    private _router: Router,
+    private _snackBar: MatSnackBar
+  ) {
+    this.taskData = {};
+    this.message = '';
   }
 
+  ngOnInit(): void {
+    this._boardService.listTask().subscribe(
+      (res)=>{
+        console.log(res);
+        this.taskData = res.board;
+      },
+      (err) => {
+        console.log(err);
+        this.message = err.error;
+        this.openSnackBarError();        
+      }
+    );
+  }
+
+  updateTask(task: any, status: string){}
+
+  deleteTask(task: any){}
+
+  openSnackBarSuccesfull() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSecond * 1000,
+      panelClass: ['style-snackBarTrue'],
+    });
+  }
+
+  openSnackBarError() {
+    this._snackBar.open(this.message, 'X', {
+      horizontalPosition: this.horizontalPosition,
+      verticalPosition: this.verticalPosition,
+      duration: this.durationInSecond * 1000,
+      panelClass: ['style-snackBarFalse'],
+    });
+  }
 }
