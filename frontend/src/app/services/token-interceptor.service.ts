535664856a23cb19f6c5a85656c6fdd464569bd7
Jairo Alzate <jairo.alzate@siesa.com>
2021-08-30 09:26:44 -0500

    Add logout

--- frontend/src/app/services/token-interceptor.service.ts
+++ frontend/src/app/services/token-interceptor.service.ts
@@ -1,21 +1,19 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor } from "@angular/common/http";
-import { UserService } from "./user.service";
+import { HttpInterceptor } from '@angular/common/http';
+import { UserService } from './user.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class TokenInterceptorService implements HttpInterceptor{
-
-  constructor(private _userService: UserService) { 
-    
-  }
+export class TokenInterceptorService implements HttpInterceptor {
+  constructor(private _userService: UserService) {}
+  
   intercept(req: any, next: any) {
     const tokenReq = req.clone({
-      setHeader: {
+      setHeaders: {
         Authorization: 'Bearer ' + this._userService.getToken(),
-      }
-    })
+      },
+    });
     return next.handle(tokenReq);
   }
 }
